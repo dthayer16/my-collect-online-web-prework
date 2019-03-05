f516745d284d1860d1a0de38652a41abf3b09d2c
David Thayer <dthayer16@gmail.com>
2019-03-05 20:27:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,5 @@
 def my_collect(array)
+<<<<<<< HEAD
   array2 = []
   i = 0
   while i < array.length
@@ -6,4 +7,11 @@ def my_collect(array)
     i += 1
   end
   array2
+=======
+  i = 0
+  while i < array.length
+    yield (array [i])
+    i += 1
+  end
+>>>>>>> da1357ea53aeb71c18590e6ce7536e7387a8f404
 end
